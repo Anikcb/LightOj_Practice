cbbc7dacdfe24fc04badb6cd69165ccf61cfa6ee
Anik chakraborty <anikchakraborty562@gmail.com>
2022-12-14 22:53:38 +0600

    Create How Many Zeroes.cpp

---
+++ Codes/How Many Zeroes.cpp
@@ -0,0 +1,201 @@
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//#include<bits/stdc++.h>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <iterator>
+#include <bitset>
+#include <assert.h>
+#include <new>
+#include <sstream>
+#include <time.h>
+
+
+//using    namespace __gnu_pbds;
+using namespace std;
+
+
+/*** Typedef ***/
+typedef long long ll;
+typedef unsigned long long ull;
+
+
+
+
+#define scln1(a) scanf("%lld",&a)
+#define scln2(a,b) scanf("%lld %lld",&a,&b)
+
+
+
+
+
+/*** Define Values ***/
+
+#define     ff                first
+#define     ss                second
+#define     re                return
+#define     MP                make_pair
+#define     pb                push_back
+#define     SZ(x)             ((ll) (x).size())
+
+
+#define     EPS               10E-10
+#define     mxx               100005
+#define     MOD               1000000007
+#define     iseq(a,b)         (fabs(a-b)<EPS)
+#define     PI                3.141592653589793238462643
+#define     HAR               0.57721566490153286060651209
+
+
+#define     Ceil(a,b)         (a+b-1)/b
+#define     gcd(a, b)         __gcd(a,b)
+#define     min3(a,b,c)       min(a,min(b,c))
+#define     max3(a,b,c)       max(a,max(b,c))
+#define     lcm(a, b)         ((a)/gcd(a,b))*(b)
+#define     min4(a,b,c,d)     min(d,min(a,min(b,c)))
+#define     max4(a,b,c,d)     max(d,max(a,max(b,c)))
+#define     input             freopen("input.txt","rt", stdin)
+#define     output            freopen("output.txt","wt", stdout)
+
+
+#define     all(v)            v.begin(),v.end()
+#define     memz(u)           memset(u, 0, sizeof u)
+#define     memn(u)           memset(u, -1, sizeof u)
+#define     ps(x,y)           fixed<<setprecision(y)<<x
+#define     for2D0(n,m)       for(ll i=0;i<n;i++)for(ll j=0;j<m;j++)
+#define     for2D1(n,m)       for(ll i=1;i<=n;i++)for(ll j=1;j<=m;j++)
+#define     Unique(X)         (X).resize(unique(all(X))-(X).begin())
+#define     get_pos(c,x)      (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define     get_pos_up(c,x)   (upper_bound(c.begin(),c.end(),x)-c.begin())
+#define     IOS               ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+
+/*** STLs ***/
+typedef vector <ll> vll;
+typedef set <ll> sll;
+typedef multiset <ll> msll;
+typedef queue <ll> qll;
+typedef stack <ll> stll;
+typedef map <ll, ll> mll;
+typedef pair <ll, ll> pll;
+typedef vector <pair <ll , ll> > vpll;
+typedef map<string,ll> msl;
+typedef priority_queue<ll> pql;
+typedef priority_queue<ll,vector<ll>,greater<ll> > npql;
+
+
+/*** BitWise Operations
+///int Set(int N,int pos){return N=N | (1<<pos);}
+///int reset(int N,int pos){return N= N & ~(1<<pos);}
+///bool check(int N,int pos){return (bool)(N & (1<<pos));}
+***/
+
+
+/*** Grids
+///const int fx[] = {+1,-1,+0,+0};
+///const int fy[] = {+0,+0,+1,-1};
+///const int fx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's move
+///const int fy[] = {-1,+1,+0,+0,+1,+1,-1,-1}; ///king's Move
+///const int fx[] = {-2,-2,-1,-1,+1,+1,+2,+2}; ///knight's move
+///const int fy[] = {-1,+1,-2,+2,-2,+2,-1,+1}; ///knight's move
+***/
+
+
+/*** Functions
+///transform(data.begin(), data.end(), data.begin(),[](unsigned char c){ return std::tolower(c); });
+///typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+///ll toint(string s){ll n=0,k=1;for(int i=s.size()-1; i>=0; i--){n += ((s[i]-'0')*k);k*=10;}return n;}
+///string tostring(ll x){string s="";while(x){s += (x%10)+'0';x/=10;}reverse(s.begin(),s.end());return s;}
+///bool sortinrev(const pair<ll,ll> &a,const pair<ll,ll> &b)return (a.first > b.first);
+///prime[]={2,4,2,4,6,2} //start loop from 29 to do prime factorization
+///auto it = lower_bound(my_multiset.begin(), my_multiset.end(), 3);
+///const auto pos = distance(my_multiset.begin(), it);
+///priority_queue< pll ,vector<pll>,greater<pll> >p;
+///lower_bound(all(v),r+1)-lower_bound(all(v),l);
+///cout<<*X.find_by_order(0)<<endl;
+///cout<<X.order_of_key(-5)<<endl;
+///set< pair<int,int> >s;
+///pair<int,int> p={3,2};
+///auto lb=lower_bound(s.begin(),s.end(),p);
+///cout<<(*lb).first<<" "<<(*lb).second<<endl;
+***/
+
+
+/*** Some Prints ***/
+#define en cout << '\n';
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+
+
+vll v;
+ll dp[2][10][80][80];
+ll a,b,k,len;
+
+ll DP(ll isSmall, ll pos, ll start, ll value)
+{
+    if(pos==len)return value;
+    if(dp[isSmall][pos][start][value]!=-1)return dp[isSmall][pos][start][value];
+
+    ll range = (isSmall==1?9:v[pos]),res=0,st;
+    if(start)st=1;
+    else st=0;
+    for(ll i=st; i<=range; i++){
+        res+=DP(isSmall | i<v[pos], pos+1, 0, (i==0)+value);
+    }
+    if(start)res+=DP(1,pos+1,1,0);
+
+    return dp[isSmall][pos][start][value]=res;
+}
+
+ll solve(ll n)
+{
+    if(n<0)return 0;
+    if(n<=9)return 1;
+    v.clear();
+    while(n){
+        v.pb(n%10);
+        n/=10;
+    }
+    reverse(all(v));
+    len=SZ(v);
+    memn(dp);
+    return DP(0,0,1,0)+1;
+}
+
+
+int main()
+{
+    ll tst=1;
+    scln1(tst);
+    for(ll tt=1; tt<=tst; tt++)
+    {
+        //code
+        scln2(a,b);
+        if(a>b)swap(a,b);
+        printf("Case %lld: ",tt);
+        ll res=solve(b);
+        res-=solve(a-1);
+        printf("%lld\n",res);
+
+
+
+    }
+
+
+    return 0;
+}
